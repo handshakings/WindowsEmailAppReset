443a5f1789bc3db4ed6e140c64638272a3de77b3
handshakings <60043564+handshakings@users.noreply.github.com>
2022-03-30 14:02:04 -0500

    Powershell comdlet to reset windows email app

---
+++ ResetEmailApp.ps1
@@ -0,0 +1,3 @@
+﻿Get-AppxPackage microsoft.windowscommunicationsapps  | Remove-AppxPackage
+Start-Sleep -Seconds 3
+Get-AppXPackage -AllUsers microsoft.windowscommunicationsapps | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml" }
